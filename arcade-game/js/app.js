591ee41644c1d7e2bedf7588660a1e13434f10e6
hadi-ramezani <hadi.ramezani@gmail.com>
2019-04-25 21:16:24 -0500

    doc: Write the documentation for the classes and methods

--- arcade-game/js/app.js
+++ arcade-game/js/app.js
@@ -3,9 +3,20 @@ const colSize = 101;
 const enemyStartX = -100
 const bugWidth = 50
 const bugHeight = 50
+const initialLives = 3;
+let minimumBugSpeed = 50;
 
-// Enemies our player must avoid
+/**
+ * @description The Enemy class to build the enemies that our player must avoid
+ */
 class Enemy {
+    /**
+     * @description The Enemy constructor method. The constructor accepts the coordinates and speed of an enemy and
+     * sets the sprite internally.
+     * @param {number} x - The x coordinate of the enemy
+     * @param {number} y - The y coordinate of the enemy
+     * @param {number} speed - The speed of the enemy
+    */
     constructor(x=50, y=50, speed=2) {
         this.sprite = 'images/enemy-bug.png';
         this.x = x;
@@ -13,50 +24,93 @@ class Enemy {
         this.speed = speed;
     }
 
+    /**
+     * @description Make the update to the enemy. To ensure, the game runs at the same speed in different computers,
+     * scale down the speed before making moving the enemy along the x direction.
+     * @param {number} dt - The value used to scale down the speed.
+     * @returns null
+    */
     update(dt) {
         this.x = this.speed * dt + this.x;
         if (this.x > 500) {
             this.x = enemyStartX;
-            this.speed = generateRandomNumber(50, 100);
+            this.speed = generateRandomNumber(minimumBugSpeed, minimumBugSpeed + 50);
         }
         this.handleCollision();
-
     }
 
+    /**
+     * @description Render the enemy using the image and coordinates. Also, render the information related to the game
+     * including the number of lives, point, and stage on the screen.
+     * @returns null
+    */
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.fillStyle = "#FF33E3";
+        ctx.font = "22px Arial";
+        ctx.fillText(`Point: ${player.point}`, 0, 40);
+        ctx.fillText(`Lives: ${player.lives}`, 200, 40);
+        ctx.fillText(`Stage: ${player.stage}`, 400, 40);
     }
 
+    /**
+     * @description Determine if a collision between the player and a bug has happened or not. If so, reset the game and
+     * take one life.
+     * @returns null
+    */
     handleCollision() {
           if (player.x < this.x + bugWidth && player.x > this.x - bugWidth &&
               player.y < this.y + bugHeight && player.y > this.y - bugHeight) {
+              player.lives -= 1;
               player.reset();
           }
-//        console.log(player.x);
     }
 }
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 
+
+/**
+ * @description The player class to build the player
+ */
 class Player {
+
+    /**
+     * @description The Player constructor method. The constructor accepts the coordinates of the player and
+     * sets the sprite internally.
+     * @param {number} x - The x coordinate of the player
+     * @param {number} y - The y coordinate of the player
+    */
     constructor(x=200, y=400) {
         this.sprite = 'images/char-boy.png';
         this.startX = x;
         this.startY = y;
         this.x = x;
         this.y = y;
+        this.point = 0;
+        this.lives = initialLives;
+        this.stage = 1;
     }
 
+    /**
+     * @description Make the update to the player.
+     * @returns null
+    */
     update(dt) {
-        // implement update here
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    /**
+     * @description Render the enemy using the image and coordinates.
+     * @returns null
+    */
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 
+    /**
+     * @description Handle the keypress by updating the coordinates of the player. Make sure the player will not move
+     * off-screen. If the player, has reached the water, make the call to handleSuccess method.
+     * @returns null
+    */
     handleInput(key) {
         switch (key ) {
             case "up":
@@ -84,20 +138,41 @@ class Player {
         }
     }
 
+    /**
+     * @description Move the player to the starting point if a collision has happened. Also, reset the game if the player
+     * runs out of lives. r of lives, point, and stage on the screen.
+     * @returns null
+    */
     reset() {
         this.x = this.startX;
         this.y = this.startY;
+        if (this.lives < 0) {
+            showModal();
+            this.point = 0;
+            this.lives = initialLives;
+            this.stage = 1;
+            minimumBugSpeed = 50;
+            generateEnemies();
+        }
     }
 
+    /**
+     * @description When the player reaches the water, increment the stage, points, and bug speed.
+     * @returns null
+    */
     handleSuccess(){
-        alert("You won");
+        this.point += this.stage;
         this.reset();
+        this.stage += 1;
+        minimumBugSpeed += 10;
+        generateEnemies();
     }
 }
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+/**
+ * @description Generate an array of enemies and put them in an array.
+ * @returns {array} - An array of Enemy objects.
+*/
 function generateEnemies(){
     let allEnemies = [];
     const enemyYPositions = [60, 60, 145, 145, 230, 230];
@@ -105,18 +180,26 @@ function generateEnemies(){
     const numEnemies = 6;
     for (let i=0; i < numEnemies; i++){
         allEnemies.push(new Enemy(enemyStartX, enemyYPositions[generateRandomNumber(0, enemyYPositions.length)],
-                                       generateRandomNumber(50, 100)));
+                                       generateRandomNumber(minimumBugSpeed, minimumBugSpeed + 50)));
     }
     return allEnemies
 }
 
+/**
+ * @description Generate a random number between two boundaries.
+ * @param {number} low - The lower limit for the random number.
+ * @param {number} high - The upper limit fot the generated random number.
+ * @returns null
+*/
 function generateRandomNumber(low=1, high=100) {
     const randomNumber = Math.floor(Math.random() * high) + low
     return randomNumber;
 }
 
+// create the objects
 allEnemies = generateEnemies();
 player = new Player();
+
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
