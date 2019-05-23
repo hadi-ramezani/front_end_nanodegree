6e3da4811b9dfcb353de89aa692f26f685d9a2a9
hadi-ramezani <hadi.ramezani@gmail.com>
2019-05-22 19:27:56 -0500

    feature: Update the fetch section in sw to show the content on refresh

--- restaurant_review_app/sw.js
+++ restaurant_review_app/sw.js
@@ -31,14 +31,33 @@ self.addEventListener('install', function(event) {
 });
 
 // Take control of the network requests
+// The code from https://developers.google.com/web/fundamentals/primers/service-workers/
 self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
-        })
-    );
-});
+   event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                // Cache hit - return response
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request).then(function(response) {
+                    // Check if we received a valid response
+                    if(!response || response.status !== 200 || response.type !== 'basic') {
+                        return response;
+                    }
 
+                    var responseToCache = response.clone();
+
+                    caches.open(staticCacheName).then(function(cache) {
+                        cache.put(event.request, responseToCache);
+                    });
+                    return response;
+
+                }
+            );
+        })
+   );
+});
 // Listen to activate events to update the caches and delete the old ones
 self.addEventListener('activate', function(event) {
     event.waitUntil(
